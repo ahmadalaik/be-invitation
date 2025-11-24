0f2a28203e5090effb67a2e8bd6bc876a4cb7229
ahmadalaik <ahmadalaik.mu@gmail.com>
2025-11-24 20:53:56 +0700

    refactor: invitation repository

--- internal/repository/invitation_repository.go
+++ internal/repository/invitation_repository.go
@@ -7,11 +7,11 @@ import (
 
 type InvitationRepository interface {
 	Create(inv *models.Invitation) error
-	FindByID(id uint) (*models.Invitation, error)
+	FindByID(id uint64) (*models.Invitation, error)
 	FindBySlug(slug string) (*models.Invitation, error)
-	FindByUserID(userID uint) ([]models.Invitation, error)
-	Update(inv models.Invitation) error
-	Delete(id uint) error
+	FindByUserID(userID uint64) ([]models.Invitation, error)
+	Update(inv *models.Invitation) error
+	Delete(id uint64) error
 }
 
 type invitationRepository struct {
@@ -28,7 +28,7 @@ func (r *invitationRepository) Create(inv *models.Invitation) error {
 }
 
 // FindByID implements InvitationRepository.
-func (r *invitationRepository) FindByID(id uint) (*models.Invitation, error) {
+func (r *invitationRepository) FindByID(id uint64) (*models.Invitation, error) {
 	var inv models.Invitation
 	if err := r.db.Preload("Guests").Preload("Template").Where("id = ?", id).First(&inv).Error; err != nil {
 		return nil, err
@@ -38,14 +38,14 @@ func (r *invitationRepository) FindByID(id uint) (*models.Invitation, error) {
 
 func (r *invitationRepository) FindBySlug(slug string) (*models.Invitation, error) {
 	var inv models.Invitation
-	if err := r.db.Preload("Guests").Preload("Template").Where("slug = ?", slug).First(&inv).Error; err != nil {
+	if err := r.db.Preload("StoryImages").Preload("Template").Where("slug = ?", slug).First(&inv).Error; err != nil {
 		return nil, err
 	}
 	return &inv, nil
 }
 
 // FindByUserID implements InvitationRepository.
-func (r *invitationRepository) FindByUserID(userID uint) ([]models.Invitation, error) {
+func (r *invitationRepository) FindByUserID(userID uint64) ([]models.Invitation, error) {
 	var invs []models.Invitation
 	if err := r.db.Where("user_id = ?", userID).Find(&invs).Error; err != nil {
 		return nil, err
@@ -54,12 +54,12 @@ func (r *invitationRepository) FindByUserID(userID uint) ([]models.Invitation, e
 }
 
 // Update implements InvitationRepository.
-func (r *invitationRepository) Update(inv models.Invitation) error {
-	return r.db.Save(&inv).Error
+func (r *invitationRepository) Update(inv *models.Invitation) error {
+	return r.db.Save(inv).Error
 }
 
 // Delete implements InvitationRepository.
-func (r *invitationRepository) Delete(id uint) error {
+func (r *invitationRepository) Delete(id uint64) error {
 	var inv models.Invitation
 	return r.db.Where("id = ?", id).Delete(&inv).Error
 }
